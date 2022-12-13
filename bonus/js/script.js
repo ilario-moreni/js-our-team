45e11ef75255c4054d74c836bd2eee58371d3f0f
ilario-moreni <ilario2001.mor@gmail.com>
2022-12-13 15:53:28 +0100

    bonus 1

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -48,10 +48,18 @@ let container = document.getElementById('my-container');
 function generateKey(container, objectArray){
     for (let i=0; i<objectArray.length; i++){
         for (let key in objectArray[i]){
+            /* bonus 1 */
+            if(key == 'image'){
+                let myImg = document.createElement('img');
+                myImg.src = `./img/${objectArray[i][key]}`;
+                console.log(myImg);
+                container.appendChild(myImg);
+            } else {
                 let myDiv = document.createElement('div');
                 myDiv.innerHTML = objectArray[i][key];
                 console.log(myDiv);
                 container.appendChild(myDiv);
+            }
         }
     }
 }
